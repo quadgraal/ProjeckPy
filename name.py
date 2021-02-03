bdf02eafd79812c4ef40d744c9de42377e95eee4
Artemiy <khoba.artemiy@icloud.com>
2021-02-03 22:29:15 +0200

    слияние

--- name.py
+++ name.py
@@ -2,4 +2,21 @@ class Abiturient :
     def __init__ ( self, fio, age ):
         self.__fio = fio
         self.__age = age
+<<<<<<< Updated upstream
+=======
+
+class Faculty :
+    def __init__ ( self, university, direction, N):
+        self.__university = university
+        self.__direction = direction
+        self.__N = N
+class Exam:
+    def __init__ ( self, level, subject ):
+        self.__level = level
+        self.__subject = subject
+class Teacher:
+    def __init__ ( self, subject, age1):
+        self.__subject = subject
+        self.__age1 = age1
+>>>>>>> Stashed changes
         
