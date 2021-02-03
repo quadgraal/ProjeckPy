0c1ea48dbf0524b7e691a97c40643e89522c2f91
Artemiy <khoba.artemiy@icloud.com>
2021-02-03 22:20:17 +0200

    Add new file (Faculty)

--- name.py
+++ name.py
@@ -2,4 +2,9 @@ class Abiturient :
     def __init__ ( self, fio, age ):
         self.__fio = fio
         self.__age = age
-        
+
+class Faculty :
+    def __init__ ( self, university, direction, N):
+        self.__university = university
+        self.__direction = direction
+        self.__N = N
