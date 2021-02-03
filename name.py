fb5f7fa3cc025291da4a1f01634f718886d36172
Artemiy <khoba.artemiy@icloud.com>
2021-02-03 22:20:17 +0200

    Add new class (Faculty)

--- name.py
+++ name.py
@@ -2,14 +2,4 @@ class Abiturient :
     def __init__ ( self, fio, age ):
         self.__fio = fio
         self.__age = age
-
-class Faculty :
-    def __init__ ( self, university, direction, N):
-        self.__university = university
-        self.__direction = direction
-        self.__N = N
-class Exam:
-    def __init__ ( self, level, subject ):
-        self.__level = level
-        self.__subject = subject
         
